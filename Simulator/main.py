d3ba667357cb899560a22c36ba036f8cf4cebb6b
pablo <pcusnir@gmail.com>
2020-03-24 10:02:42 +0200

    Updated README. Updated local_main.py

--- Simulator/main.py
+++ Simulator/main.py
@@ -1,15 +1,8 @@
 from Simulator.dir_reader import DirReader
 from Simulator.configure import Configure
 from Simulator.server import app
-from Test.ep_simulator import ep_simulator
-import os
-from pathlib import Path
-from threading import Thread
-import queue
 
-q = queue.Queue()
 configuration = Configure()
 d = DirReader(configuration)
 d.start()
-q.put("something in the queue")
 app.run(debug=False, host='0.0.0.0', port=5000)
--- local_main.py
+++ local_main.py
@@ -1,5 +1,6 @@
 from Simulator.dir_reader import DirReader
 from Simulator.server import app
+from Simulator.configure import Configure
 from Test.ep_simulator import ep_simulator
 from pathlib import Path
 from threading import Thread
@@ -10,7 +11,8 @@ def start_ep_simulator():
     ep_simulator.run(debug=False, host='0.0.0.0', port=5050)
 
 
-d = DirReader(Path(os.curdir + "/Test/files_from_SP/"), 1)
+configuration = Configure()
+d = DirReader(configuration)
 d.start()
 Thread(target=start_ep_simulator).start()
 app.run(debug=False, host='0.0.0.0', port=5000)
